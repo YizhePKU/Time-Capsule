fd85e8f6321ecd65eff9cb130533e1c108c474dc
YizhePKU <yizhe@pku.edu.cn>
2021-04-04 20:15:17 +0800

    Wrapping the ledger with an API

---
+++ ledger.py
@@ -0,0 +1,38 @@
+import grpc
+
+from google.protobuf.empty_pb2 import Empty as EmptyRequest
+from bdware.bdledger.api.query_pb2_grpc import QueryStub
+from bdware.bdledger.api import query_pb2
+from bdware.bdledger.api.ledger_pb2_grpc import LedgerStub
+from bdware.bdledger.api import ledger_pb2
+
+ledger = 'time_capsule'
+hello_world_key = b'\xb3\xeak\xaf\xa4h\xb4\xfa\xc7\x9d\x07\xe58\x1a\xa7\xdd\xc5\x0f\x06\x9e'
+
+chan = grpc.insecure_channel('39.104.201.40:21121')
+ledger_stub = LedgerStub(chan)
+query_stub = QueryStub(chan)
+
+def add(data: bytes) -> bytes:
+    '''Add data to the ledger.
+    Returns a key that can be used to retrive the data from the ledger.'''
+    # Most fields of the transaction can be left blank
+    transaction = ledger_pb2.SendTransactionRequest.Transaction(
+        data=data
+    )
+    request = ledger_pb2.SendTransactionRequest(
+        ledger=ledger,
+        transaction=transaction,
+    )
+    response = ledger_stub.SendTransaction(request)
+    return response.hash
+
+def get(key: bytes) -> bytes:
+    '''Retrive data from the ledger.
+    Throws KeyError if no data is associated with given key.'''
+    request = query_pb2.GetTransactionByHashRequest(
+        ledger=ledger,
+        hash=key,
+    )
+    response = query_stub.GetTransactionByHash(request)
+    return response.transaction.data
